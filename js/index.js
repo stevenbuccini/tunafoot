c6f0b4db3db49181d264c83e56dad8a69ca32109
Alex Triana <alex.triana2@gmail.com>
2014-07-19 15:48:58 -0700

    start of random questions

--- js/index.js
+++ js/index.js
@@ -1,13 +1,52 @@
+Parse.initialize("ZvuYGWmttHsaBJ0UDvxK76s7cstAwNRzh4SYDDLV", "KmJ694MObD0g26KVUyF2L8esD1MwTR76hMWJhfUA");
 
-   Parse.initialize("ZvuYGWmttHsaBJ0UDvxK76s7cstAwNRzh4SYDDLV", "KmJ694MObD0g26KVUyF2L8esD1MwTR76hMWJhfUA");
-    
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-      testObject.save({foo: "bar"}, {
-      success: function(object) {
-        $(".success").show();
+var Sentence = Parse.Object.extend("Sentence");
+var Question = Parse.Object.extend("Question");
+var queryQuestion = new Parse.Query(Question);
+var querySentence = new Parse.Query(Sentence);
+
+//////////////////////////////////////////
+// VARIABLES AND FUNCTION DECLARATIONS ///
+//////////////////////////////////////////
+
+var TOTAL_QUESTIONS = 7;
+var QUESTIONS_PER_USER = 5;
+
+function _getRandomNumbersList() {
+  var arr = []
+  while (arr.length <= QUESTIONS_PER_USER) {
+    var newNumber = Math.floor(Math.random() * TOTAL_QUESTIONS);
+    (newNumber in arr) ? "E" : arr.push(newNumber);
+  }
+  return arr;
+}
+
+function getQuestions() {
+  var questionIds = _getRandomNumbersList();
+  var questions = [];
+  // queryQuestion.equalTo("numId", ;
+  queryQuestion.find({
+    success: function (results) {
       },
-      error: function(model, error) {
-        $(".error").show();
+      error: function (error) {
+        alert("this should never happen you are fucked.")
       }
     });
+
+  for (var j = 0; j < QUESTIONS_PER_USER; j++) {
+   
+  }
+}
+
+function init() {
+  var questions = getQuestions();
+  console.log(questions);
+  
+}
+
+
+//////////////////////////////////////////
+//           Initialization            ///
+//////////////////////////////////////////
+
+init();
