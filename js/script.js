7ab17c51d4899acdd1e15950eceeca79ab0238de
Steven Buccini <steven@Stevens-MacBook-Air.local>
2014-07-20 01:21:59 -0700

    Included Underscore.js library. Removed index.js, moved all functionality to script.js and cleaned up that codebase.

--- js/script.js
+++ js/script.js
@@ -1,81 +1,3 @@
-$(document).ready(function() {
-
-    var questions = [
-        "Who are you?",
-        "Where do you come from and why?",
-        "What's your favorite thing?",
-        "What's the most dangerous thing you own?",
-        "What is the folly of man?"];
-
-    var answers = [];
-
-    //set variable for #of questions and responses
-    var i=0;
-
-    //sets question in placeholder prompt
-    $("#text").attr("placeholder", questions[i]);
-
-    var value="";
-
-    /*var questions = getQuestions(),
-    answers = [],
-    i=0,*/
-
-
-
-
-    //next question function- next question,resets input on click + enter
-    function nextQuestion(){
-        if (value.length>0){
-        	if (i<4){
-    	    	i++;
-    	        $("#text").attr("placeholder", questions[i]);
-    	        console.log(value, i);
-    	        answers.push(value);//pushes response to answers array
-    	        console.log(answers);
-    	        $("#text").attr("value","");
-        	} else if (i===4) {
-                //fifth one
-                i++,
-                console.log(value, i);
-                answers.push(value);
-                console.log(answers);
-                $("#text").attr("value","");
-                $("input").addClass("sentence-input");
-    			$("#text").attr("placeholder", "Write the next sentence using your answers.");
-    			$("#text").attr("maxlength", "180");
-                document.getElementById('wordbank').innerHTML = answers.join(" ");
-                $("#lastsentence").fadeIn();
-                $("#wordbank").fadeIn();
-    		} 
-        }
-    };
-
-    //updating value with every keypress
-    $("#text")
- 		.keyup(function() {
-    	value = $(this).val();
-  		});
-  		
- 	//on button click, do next question
-    $('#button').click(function(){   
-    	nextQuestion();
-
-    });
-
-    //on enter keypress, do the same shit as button click
-	$(document).keypress(function(event){
-		var keycode = (event.keyCode ? event.keyCode : event.which);
-		if(keycode == '13'){
-			//do this code when enter is pressed, first line prevents page refresh
-			event.preventDefault();
-			nextQuestion();	
-		}
-	});
-
-
-
-//old index.js
 
 Parse.initialize("ZvuYGWmttHsaBJ0UDvxK76s7cstAwNRzh4SYDDLV", "KmJ694MObD0g26KVUyF2L8esD1MwTR76hMWJhfUA");
 
@@ -108,7 +30,6 @@ function _getRandomNumbersList() {
 function _isInArray(value, array) {
   return array.indexOf(value) > -1;
 }
-
 
 
 function getQuestions() {
@@ -186,13 +107,79 @@ function fetchSentences() {
   });
 }
 
+$(document).ready(function() {
+
+    var questions = [
+        "Who are you?",
+        "Where do you come from and why?",
+        "What's your favorite thing?",
+        "What's the most dangerous thing you own?",
+        "What is the folly of man?"];
+
+    var answers = [];
+
+    //set variable for #of questions and responses
+    var i=0;
+
+    //sets question in placeholder prompt
+    $("#text").attr("placeholder", questions[i]);
+
+    var value="";
+
+    /*var questions = getQuestions(),
+    answers = [],
+    i=0,*/
 
 
 
 
+    //next question function- next question,resets input on click + enter
+    function nextQuestion(){
+        if (value.length>0){
+        	if (i<4){
+    	    	i++;
+    	        $("#text").attr("placeholder", questions[i]);
+    	        console.log(value, i);
+    	        answers.push(value);//pushes response to answers array
+    	        console.log(answers);
+    	        $("#text").attr("value","");
+        	} else if (i===4) {
+                //fifth one
+                i++,
+                console.log(value, i);
+                answers.push(value);
+                console.log(answers);
+                $("#text").attr("value","");
+                $("input").addClass("sentence-input");
+    			$("#text").attr("placeholder", "Write the next sentence using your answers.");
+    			$("#text").attr("maxlength", "180");
+                document.getElementById('wordbank').innerHTML = answers.join(" ");
+                $("#lastsentence").fadeIn();
+                $("#wordbank").fadeIn();
+    		} 
+        }
+    };
 
+    //updating value with every keypress
+    $("#text")
+ 		.keyup(function() {
+    	value = $(this).val();
+  		});
+  		
+ 	//on button click, do next question
+    $('#button').click(function(){   
+    	nextQuestion();
 
+    });
 
-
+    //on enter keypress, do the same shit as button click
+	$(document).keypress(function(event){
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '13'){
+			//do this code when enter is pressed, first line prevents page refresh
+			event.preventDefault();
+			nextQuestion();	
+		}
+	});
 
 });
