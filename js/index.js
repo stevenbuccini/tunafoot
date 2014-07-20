2a46640e403cb672061acb7424e4f60e3c26e0d0
Alex Triana <alex.triana2@gmail.com>
2014-07-19 23:33:13 -0700

    sentence validation

--- js/index.js
+++ js/index.js
@@ -16,42 +16,72 @@ function _getRandomNumbersList() {
   var arr = []
   while (arr.length <= QUESTIONS_PER_USER) {
     var newNumber = Math.floor(Math.random() * TOTAL_QUESTIONS);
-    (newNumber in arr) ? "E" : arr.push(newNumber);
+    (_isInArray(newNumber, arr)) ? "hacky" : arr.push(newNumber);
   }
   return arr;
 }
 
+function _isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+
+
+
 function getQuestions() {
-  var questionIds = _getRandomNumbersList();
-  var questions = [];
-  // queryQuestion.equalTo("numId", ;
-  queryQuestion.notEqualTo("numId", "99");
+  var questionIds = _getRandomNumbersList(),
+  questions = [],
+  object,
+  index;
+
+  queryQuestion.notEqualTo("numId", 99);
+
   queryQuestion.find({
     success: function (results) {
-      for (var i = 0; i < results.length; i++) { 
-        var object = results[i];
-        alert(object.get('content'));
+      for (var i = 0; i < QUESTIONS_PER_USER; i++) { 
+        index = questionIds[i]
+        object = results[index];
+        appendToQuestionsList(object.get('content'));
       }
-      },
+    },
       error: function (error) {
         alert("this should never happen you are fucked.")
       }
     });
 
-  for (var j = 0; j < QUESTIONS_PER_USER; j++) {
-   
+  function appendToQuestionsList(newQuestion) {
+    questions.push(newQuestion);
   }
-}
 
-function init() {
-  var questions = getQuestions();
-  console.log(questions);
-  
+  return function() { return questions};
 }
 
 
-//////////////////////////////////////////
-//           Initialization            ///
-//////////////////////////////////////////
+$('#sentence-input').keypress(function() {
+  var answers = ["dog","blue"];
+    var el = this;
+    if (this.timeoutId)
+        window.clearTimeout(this.timeoutId);
+    this.timeoutId = window.setTimeout(function () {
+      var input = $(el).val().split(" ");
+      if (isValidNewSentence(answers, input)) {
+        alert("Got the words!");
+      }
+    }, 200);
+});
 
-init();
+function isValidNewSentence(answers, input) {
+  var total=0;
+  for (var i = 0; i<input.length; i++) {
+    for (var k = 0; k < answers.length; k++) {
+      (input[i] == answers[k]) ? total++ : "hacky";
+    }
+    if (total >= answers.length) {
+      return true
+    }
+  }
+  return false
+}
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
-$( document ).ready(function() {
 
-    var questions = [
-    	"Who are you?",
-   		"Where do you come from and why?",
-    	"What's your favorite thing?",
-    	"What's the most dangerous thing you own?",
-    	"What is the folly of man?"];
 
-    var answers = [];
-
-    //set variable for #of questions and responses
-    var i=0;
+    var questions = getQuestions(),
+    answers = [],
+    i=0,
+    value="";
 
     //sets question in placeholder prompt
     $("#text").attr("placeholder", questions[i]);
 
-    var value="";
+
 
 
     //next question function- next question,resets input on click + enter
@@ -23,14 +16,14 @@ $( document ).ready(function() {
     	if (i<=3){
 	    	i++;
 	        console.log(i);
-	        $("#text").attr("placeholder", questions[i]);
+	        $("#text").attr("placeholder", questions()[i]);
 	        console.log(value);
 	        answers.push(value);//pushes response to answers array
 	        console.log(answers);
 	        $("#text").attr("value","");
     	} else {
     		//once the first 5 are done
-			$("#text").attr("placeholder", "Write the next sentance using your answers.");
+			$("#text").attr("placeholder", "Write the next sentence using your answers.");
 			$("#text").attr("maxlength", "140");
 			
 
@@ -74,4 +67,3 @@ $( document ).ready(function() {
 
 
 
-});
